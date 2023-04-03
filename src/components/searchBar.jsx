1022bb876c27542ab9695d1b594180aeb2c35982
Anders Flotten <anders3835@hotmail.com>
2023-04-03 21:10:39 +0000

    Set up search bar, merge for pull request

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <form className = "search-container">
+     
+      <input type="text" placeholder="Search for exercise by muscle group" name="search"/>
+    </form>
+  )
+}
+
+export default SearchBar;
---
+++ src/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from '@/components/SearchBar';
+
+function HomePage() {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
+}
+
+export default HomePage;
