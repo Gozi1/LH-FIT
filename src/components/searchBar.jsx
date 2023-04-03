7024dd69cb60ab513c3ece469b66ff5d1d3482ad
Anders Flotten <anders3835@hotmail.com>
2023-04-03 23:22:29 +0000

    Implement search bar basics

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import styles from '../styles/search.module.css'
 
 const SearchBar = () => {
   return (
-    <form className = "search-container">
-     
-      <input type="text" placeholder="Search for exercise by muscle group" name="search"/>
+    <form className={styles['search-form']}>
+      <div className={styles['search-bar']}>
+        <input placeholder='Search by muscle group' className={styles['search-bar']}></input>
+        </div>
+        <div>
+        <button className={styles['search-button']}>Search</button>
+      </div>
     </form>
+
+
   )
 }
 
