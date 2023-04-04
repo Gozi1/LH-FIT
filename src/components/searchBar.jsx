5048cf802926b577e737511f34c70f163f3705cc
Anders Flotten <anders3835@hotmail.com>
2023-04-04 00:01:06 +0000

    search bar touch up

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import styles from '../styles/search.module.css'
 
 const SearchBar = () => {
+  // search to api
+  // get a varible that saves whats in search bar
+  // make a function that handles submit request/takes response data (search by name - stretch)
+  // 
+
+
   return (
     <form className={styles['search-form']}>
-      <div className={styles['search-bar']}>
-        <input placeholder='Search by muscle group' className={styles['search-bar']}></input>
-        </div>
-        <div>
-        <button className={styles['search-button']}>Search</button>
-      </div>
+      <input placeholder='Search by muscle group' className={styles['search-bar']}></input>
+      <button className={styles['search-button']}>Search</button>
     </form>
 
 
