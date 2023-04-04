ce8b322fc52722a1331aa6798c04dcb5d23d6340
Anders Flotten <anders3835@hotmail.com>
2023-04-04 02:20:39 +0000

    Implement search bar functionality

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
-import styles from '../styles/search.module.css'
+import { React, useEffect, useState } from 'react';
+import styles from '../styles/search.module.css';
+import axios from "axios";
 
 const SearchBar = () => {
+  const [params, setParams] = useState({
+    difficulty: "beginner",
+    type: '',
+    muscle: '',
+    offset: 6,
+    name: '',
+    exercises: []
+  })
+ const [input, setInput] = useState("")
+
+ const handleSubmit = () => {
+  setParams(prev=>({...prev, name: input}));
+  setInput("")
+ }
+
+ 
+  useEffect(() => {
+    if(params.name) {
+    const options = {
+      method: 'GET',
+      url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises',
+      params: { name: params.name, muscle: '', type: '' },
+      headers: {
+        'X-RapidAPI-Key': '87b5ef7ecemsh3744aa57e06b7f7p1b5fffjsn04eef49b9efc',
+        'X-RapidAPI-Host': 'exercises-by-api-ninjas.p.rapidapi.com'
+      }
+    }
+
+    axios.request(options).then(function (response) {
+      // console.log(response.data);
+      setParams(prev => ({ ...prev, exercises: response.data }));
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+  }, [params.name])
+
+
   return (
-    <form className={styles['search-form']}>
+    <main>
+    <form className={styles['search-form']} onSubmit={event => event.preventDefault()} autoComplete='off' >
       <div className={styles['search-bar']}>
-        <input placeholder='Search by muscle group' className={styles['search-bar']}></input>
-        </div>
-        <div>
-        <button className={styles['search-button']}>Search</button>
+        <input placeholder='Search by muscle group' type='text' value={input} 
+        onChange={(e) => setInput(e.target.value)} className={styles['search-bar']}></input>
+      </div>
+      <div>
+        <button className={styles['search-button']} onClick={handleSubmit}>Search</button>
       </div>
     </form>
-
-
+      <div>
+        {(params.exercises).map((exercise, i) => <div key={i} className={"search-result"}>{exercise.name}</div>)}
+      </div>
+    </main>
   )
 }
 
