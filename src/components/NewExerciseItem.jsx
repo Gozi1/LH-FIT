945ea49136b95313a72ee71be3a109f6006da026
Gozi1 <82515026+Gozi1@users.noreply.github.com>
2023-04-05 16:52:56 -0400

    remove unneccessary files

--- src/components/NewExerciseItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewExerciseItem = () => {
-  return (
-    <div>NewExerciseItem</div>
-  )
-}
-
-export default NewExerciseItem
--- src/components/NewExerciseList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewExerciseList = () => {
-  return (
-    <div>NewExerciseList</div>
-  )
-}
-
-export default NewExerciseList
