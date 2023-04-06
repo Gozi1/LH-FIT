5357435e36124307bb5a7fe2b627ef73ff0095ab
Gozi1 <82515026+Gozi1@users.noreply.github.com>
2023-04-06 00:46:31 -0400

    search ,nav links and functionality updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,62 @@
-import React from 'react'
-import {BsSearch,BsList,BsX} from 'react-icons/bs'
-import { useState } from 'react'
+import React from 'react';
+import { BsSearch, BsList, BsX } from 'react-icons/bs';
+import { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
 const navbar = () => {
-  const [mobileMenu,setMobileMenu] = useState(false)
-  return (
-    <nav className = 'nav'> 
-  <div className='mobile-menu'>
-      
-    {!mobileMenu && <BsList onClick={()=>setMobileMenu(true)}/>}
-    {mobileMenu && <BsX onClick={()=>setMobileMenu(false)}/> }
-    <a className='mobile-icon'>Menu</a>
-    
-    <ul className = {mobileMenu ? 'mobile-menu-show':'mobile-menu-hide'}>
-    <li><a>Create Workout</a></li>
-    <li><a>Explore</a></li>
-    <li><a>Login</a></li>
-    <li><a>SignUp</a></li>
-    </ul> 
-    </div>  
-   </nav> 
-  )
-}
+	let router = useRouter();
+	let menuRef = useRef();
+	const [mobileMenu, setMobileMenu] = useState(false);
+	useEffect(() => {
+      let handler = (e) =>{
+        if(!menuRef.current.contains(e.target)){
+          setMobileMenu(false)
+        }
+      }
+      document.addEventListener('mousedown',handler)
+  });
+	return (
+		<nav className='nav'>
+			<div className='mobile-menu' ref={menuRef}>
+				{!mobileMenu && <BsList onClick={() => setMobileMenu(true)} />}
+				{mobileMenu && <BsX onClick={() => setMobileMenu(false)} />}
+				<a className='mobile-icon'>Menu</a>
 
-export default navbar
+				<ul
+					className={mobileMenu ? 'mobile-menu-show' : 'mobile-menu-hide'}
+					
+				>
+					<li
+						onClick={() => {
+							router.push('/create-a-workout');
+						}}
+					>
+						Create Workout
+					</li>
+					<li
+						onClick={() => {
+							router.push('/search');
+						}}
+					>
+						Explore
+					</li>
+					<li
+						onClick={() => {
+							router.push('/login');
+						}}
+					>
+						Login
+					</li>
+					<li
+						onClick={() => {
+							router.push('/signup');
+						}}
+					>
+						SignUp
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default navbar;
