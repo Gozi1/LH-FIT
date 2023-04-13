9f084700a5777f4d589946f8f971e48d4e49a9f3
William Li <110069721+william-li0128@users.noreply.github.com>
2023-04-12 22:26:55 -0700

    render the explore page

--- src/components/ExplorePage.jsx
+++ src/components/ExplorePage.jsx
@@ -1,13 +1,12 @@
 import { React, useState, useEffect } from "react";
-import SearchItem from './SearchItem';
 import axios from "axios";
 import { useCookies } from 'react-cookie';
-import getEnrollmentsForRoutine from '../helpers/selectors';
+import ExerciseItem from './ExerciseItem';
 
 const ExplorePage = () => {
   const [cookies,setCookies] = useCookies(['user']);
 
-  const [routines,setRoutines] = useState({});
+  const [routines,setRoutines] = useState();
 
   const userEmail = decodeURIComponent(cookies.user);
 
@@ -22,9 +21,9 @@ const ExplorePage = () => {
           params: { email: userEmail }
         }
         const routineResponses = await axios.request(options);
-        const routines = routineResponses.data
+        const routineResults = routineResponses.data
         
-        const enrollmentPromises = routines.map((routine) => axios.get(`http://localhost:8080/api/enrollments/routine/${routine.id}`));
+        const enrollmentPromises = routineResults.map((routine) => axios.get(`http://localhost:8080/api/enrollments/routine/${routine.id}`));
         const enrollmentResponses = await Promise.all(enrollmentPromises);
         const enrollments = enrollmentResponses.map((response) => response.data);
 
@@ -51,18 +50,86 @@ const ExplorePage = () => {
     <div>
       <h1>Explore:</h1>
       <div>
-        <h3>{routines[0].name}</h3>
-          <h5>name: {routines[0][0][0].exercise.name}</h5>
-          <h5>muscle: {routines[0][0][0].exercise.muscle}</h5>
-          <h5>type: {routines[0][0][0].exercise.type}</h5>
-          <h5>difficulty: {routines[0][0][0].exercise.difficulty}</h5>
-          <h5>sets: {routines[0][0][0].sets}</h5>
-          <h5>reps: {routines[0][0][0].reps}</h5>
-          <h5>weight: {routines[0][0][0].weight}</h5>
-        <h3>{routines[1].name}</h3>
-        <h3>{routines[2].name}</h3>
-        <h3>{routines[3].name}</h3>
-        <h3>{routines[4].name}</h3>
+        <h3>{routines[0] && routines[0].name}</h3>
+        {routines[0][0].map(enrollment => {
+          return(
+            <div key={enrollment.exercise.name}>
+            <ExerciseItem
+              name={enrollment.exercise.name}
+              sets={enrollment.sets}
+              reps={enrollment.reps}
+              muscleGroup={enrollment.exercise.muscle}
+              equipment={enrollment.exercise.equipment}
+              instructions={enrollment.exercise.instructions}
+            />
+            <br />
+          </div>   
+          )
+        })}
+        <h3>{routines[1] && routines[1].name}</h3>
+          {routines[1][0].map(enrollment => {
+            return(
+              <div key={enrollment.exercise.name}>
+              <ExerciseItem
+                name={enrollment.exercise.name}
+                sets={enrollment.sets}
+                reps={enrollment.reps}
+                muscleGroup={enrollment.exercise.muscle}
+                equipment={enrollment.exercise.equipment}
+                instructions={enrollment.exercise.instructions}
+              />
+              <br />
+            </div>   
+            )
+          })}
+        <h3>{routines[2] && routines[2].name}</h3>
+          {routines[2][0].map(enrollment => {
+            return(
+              <div key={enrollment.exercise.name}>
+              <ExerciseItem
+                name={enrollment.exercise.name}
+                sets={enrollment.sets}
+                reps={enrollment.reps}
+                muscleGroup={enrollment.exercise.muscle}
+                equipment={enrollment.exercise.equipment}
+                instructions={enrollment.exercise.instructions}
+              />
+              <br />
+            </div>   
+            )
+          })}
+        <h3>{routines[3] && routines[3].name}</h3>
+          {routines[3][0].map(enrollment => {
+              return(
+                <div key={enrollment.exercise.name}>
+                <ExerciseItem
+                  name={enrollment.exercise.name}
+                  sets={enrollment.sets}
+                  reps={enrollment.reps}
+                  muscleGroup={enrollment.exercise.muscle}
+                  equipment={enrollment.exercise.equipment}
+                  instructions={enrollment.exercise.instructions}
+                />
+                <br />
+              </div>   
+              )
+            })}
+        <h3>{routines[4] && routines[4].name}</h3>
+          {routines[4][0].map(enrollment => {
+              return(
+                <div key={enrollment.exercise.name}>
+                <ExerciseItem
+                  name={enrollment.exercise.name}
+                  sets={enrollment.sets}
+                  reps={enrollment.reps}
+                  muscleGroup={enrollment.exercise.muscle}
+                  equipment={enrollment.exercise.equipment}
+                  instructions={enrollment.exercise.instructions}
+                />
+                <br />
+              </div>   
+              )
+            })}
 			</div>
     </div>
   );
