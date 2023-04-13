1ff934c7f88aae60971d64dbcdfbcb2a0426c563
gozi1 <snwabiani@gmail.com>
2023-04-12 22:45:31 -0400

    sign in and nav  working

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import { BsSearch, BsList, BsX } from 'react-icons/bs';
+import {  BsList, BsX } from 'react-icons/bs';
 import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import { useCookies } from 'react-cookie';
-
+import axios from 'axios';
 const navbar = () => {
-	const router = useRouter();
+	const {push} = useRouter();
 	const menuRef = useRef();
-	const [cookies,setCookies] = useCookies(['user']);
-	
+	const [cookies,setCookies] = useCookies(['user_id']);
+	const [user,setUser] = useState()
 
 	const [mobileMenu, setMobileMenu] = useState(false);
+
+
+
 	useEffect(() => {
+		//  user in the nav menu credentials
+		if(cookies['user_id']){
+			const options = {
+      method: 'GET',
+      url: `http://localhost:8080/api/users/${cookies['user_id']}`,
+      // params: {id:cookies['user_id']}
+    }
+    axios.request(options)
+      .then((response) => {
+        setUser(response.data[0].name)
+      }).catch((error) => {
+				console.log(error.response.data.message)
+			});}
+	},[])
+
+
+	useEffect(() => {
+		
+			//handles the click outside of the menu
       let handler = (e) =>{
         if(!menuRef.current.contains(e.target)){
           setMobileMenu(false)
@@ -19,9 +41,10 @@ const navbar = () => {
       }
       document.addEventListener('mousedown',handler)
   },[mobileMenu]);
+
 	return (
-		<nav className='nav'>
-			<div className='mobile-menu' ref={menuRef}>
+		<nav className='nav' ref={menuRef}>
+			<div className='mobile-menu' >
 				{!mobileMenu && <BsList onClick={() => setMobileMenu(true)} />}
 				{mobileMenu && <BsX onClick={() => setMobileMenu(false)} />}
 				<a className='mobile-icon'>Menu</a>
@@ -32,29 +55,29 @@ const navbar = () => {
 				>
 					<li
 						onClick={() => {
-							router.push('/create-a-workout');
+							push('/create-a-workout');
 						}}
 					>
 						Create Workout
 					</li>
-					{cookies.user && <li>Hello {cookies.user}!</li>}
+					{user&& <li>Hello {user}!</li>}
 					<li
 						onClick={() => {
-							router.push('/search');
+							push('/search');
 						}}
 					>
 						Explore
 					</li>
 					<li
 						onClick={() => {
-							router.push('/login');
+							push('/login');
 						}}
 					>
 						Login
 					</li>
 					<li
 						onClick={() => {
-							router.push('/signup');
+							push('/signup');
 						}}
 					>
 						SignUp
